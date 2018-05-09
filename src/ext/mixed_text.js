a38f38151a22d14bc2a60572473f20c612505c22
Michael Petter <michael@petterisbetter.com>
2018-05-08 23:24:41 -0500

    clean up mixed text a bit

--- src/ext/mixed_text.js
+++ src/ext/mixed_text.js
@@ -1,7 +1,7 @@
 export const createRenderer = renderers =>
-  status => status.map((el, i) => {
-    if(typeof(el) !== 'object')
-      return el;
-    const key = Object.keys(el)[0];
-    return renderers[key](i, el[key]);
+  parts => parts.map((part, i) => {
+    if(typeof(part) !== 'object')
+      return part;
+    const [key, value] = Object.entries(part)[0];
+    return renderers[key](i, value);
   });
