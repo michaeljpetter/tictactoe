79ec887f7e99ed38b88548a466b20adc14f77a2b
Michael Petter <michael.petter@asynchrony.com>
2020-02-28 10:20:32 -0600

    add support for redux devtools

--- src/boot/store.js
+++ src/boot/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
 export default () =>
   createStore(
     reducers,
-    applyMiddleware(thunk)
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    )
   );
