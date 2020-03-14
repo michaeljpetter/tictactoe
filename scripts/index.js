ccb68c8514c8574e1a66e1d667dfaecc365627d1
Michael Petter <michael.petter@asynchrony.com>
2020-03-14 12:51:46 -0500

    enhance index script with modes option; add ability to combine reducers

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,84 @@
 'use strict';
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const paths = require('../config/paths');
+const to = require('to-case');
+const { flow, cond, map, filter, pickBy, keys, size, eq, isEmpty, startsWith, values, head, invokeArgs, add, join, T, __ } = require('lodash/fp');
 
 process.on('uncaughtException', err => {
   console.log(`\n${chalk.red(err)}\n`);
   process.exit(1);
 });
 
-const path = require('path');
-const chalk = require('chalk');
-const fs = require('fs-extra');
-const paths = require('../config/paths');
-const to = require('to-case');
-
 const targetDir = path.resolve(paths.appSrc, process.argv[2]);
 const indexPath = path.resolve(targetDir, 'index.js');
+const mode = process.argv[3] || 'js';
+
+const omitExt = new Set(['js']);
+
+const { types, mapper } =
+  flow(
+    pickBy((_, key) => startsWith(mode, key)),
+    cond([
+      [flow(size, eq(1)), flow(values, head)],
+      [isEmpty, () => { throw new Error(`unknown mode: '${mode}'`); }],
+      [T, m => { throw new Error(`ambiguous mode: '${mode}' (matches '${join(`', '`, keys(m))}')`); }],
+    ])
+  )({
+    js: {
+      types: ['js'],
+      mapper: map(([base, file]) =>
+        `export { default as ${to.camel(base)} } from './${file}';`
+      )
+    },
+    components: {
+      types: ['js', 'svg'],
+      mapper: map(([base, file]) =>
+        `export { default as ${to.pascal(base)} } from './${file}';`
+      )
+    },
+    fonts: {
+      types: ['woff', 'woff2'],
+      mapper: map(([base, file]) =>
+        `export { default as ${to.camel(base)} } from './${file}';`
+      )
+    },
+    reducers: {
+      types: ['js'],
+      mapper: files => [].concat(
+        `import { combineReducers } from 'redux';`,
+        map(([base, file]) => `import ${to.camel(base)} from './${file}';`, files),
+        ``,
+        `export default combineReducers({`,
+        flow(map(([base]) => `  ${to.camel(base)}`), join(`,\n`))(files),
+        `});`
+      )
+    }
+  });
 
 console.log(`Building ${chalk.magenta('index.js')} for ${chalk.yellow(targetDir)}:`);
 
-const types = {
-  js: { case: to.camel, omitExt: true },
-  svg: { case: to.pascal },
-  woff2: { case: to.camel }
-};
-
-const index = fs
-  .readdirSync(targetDir)
-  .filter(f => f.match(`^((?!index).)*\.(${Object.keys(types).join('|')})$`))
-  .map(f => {
+const index = flow(
+  invokeArgs('readdirSync', [targetDir]),
+  filter(f =>
+    f.match(`^((?!index).)*\.(${join('|', types)})$`) ||
+    fs.existsSync(path.join(targetDir, f, 'index.js'))
+  ),
+  map(f => {
     const ext = path.extname(f);
     const base = path.basename(f, ext);
-    const type = types[ext.replace(/^./, '')];
+    const file = omitExt.has(ext.replace(/^./, '')) ? base : base + ext;
 
-    const name = type.case(base);
-    const file = type.omitExt ? base : base + ext;
-
-    return `export { default as ${name} } from './${file}';`;
-  })
-  .join('\n');
+    return [base, file];
+  }),
+  mapper,
+  join('\n'),
+  add(__, '\n')
+)(fs);
 
 console.log(`\n${index}`);
 
 fs.writeFileSync(indexPath, index);
 
-console.log(`\n${chalk.green(`Wrote '${indexPath}'.`)}`);
+console.log(chalk.green(`Wrote '${indexPath}'.`));
