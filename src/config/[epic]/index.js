4dddf41bddbf30608f842dae04b550bfdcf6b199
Michael Petter <michael@petterisbetter.com>
2020-05-01 00:32:01 -0500

    reorder config epics so that values are adjusted in dependency order
    otherwise the changes can be inconsistent due to intermediate 'invalid' state

--- src/config/[epic]/index.js
+++ src/config/[epic]/index.js
@@ -1,10 +1,10 @@
 import { combineEpics } from 'redux-observable';
-import clampPlayers from './clamp_players';
-import clampToWin from './clamp_to_win';
 import lockDim from './lock_dim';
+import clampToWin from './clamp_to_win';
+import clampPlayers from './clamp_players';
 
 export default combineEpics(
-  clampPlayers,
+  lockDim,
   clampToWin,
-  lockDim
+  clampPlayers
 );
